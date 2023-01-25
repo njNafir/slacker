7364fa76de8c6e66a02b5f61d6867889c03f2312
Nj Nafir <njnafir@gmail.com>
2023-01-25 19:42:48 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from slacker import Slacker
 
-slack = Slacker('xoxp-1590896034022-1597673179266-3977416434902-fdd26c7b22b7e619e230f00ba3caf46c')
+slack = Slacker('')
 
 # Send a message to #general channel
 slack.chat.post_message('#general', 'Hello fellow slackers!')
